76a51a25125ae9db9159a5bb45df890c6c2ad498
dsfjsalgjdka <111098085+dsfjsalgjdka@users.noreply.github.com>
2022-11-10 13:42:09 +0900

    1110(글쓰기게시판)

---
+++ Assets/Scripts/Board/PredictBoardManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//inputfield 에 입력된 text값 저장하기
+//저장된 text값을 연결된 DB에 보내기
+//DB에 있는 값과 유니티에 입력된 text값이 일치하면 그와 일치하는 이미지값 띄우기
+
+public class PredictBoardManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
