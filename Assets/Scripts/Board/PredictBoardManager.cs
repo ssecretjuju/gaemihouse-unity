a2237df16575c6681b3c1239ff74c968990fdd92
dsfjsalgjdka <111098085+dsfjsalgjdka@users.noreply.github.com>
2022-11-10 13:45:35 +0900

    .

--- Assets/Scripts/Board/PredictBoardManager.cs
+++ Assets/Scripts/Board/PredictBoardManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 //inputfield 에 입력된 text값 저장하기
 //저장된 text값을 연결된 DB에 보내기
 //DB에 있는 값과 유니티에 입력된 text값이 일치하면 그와 일치하는 이미지값 띄우기
-
+//이미지 띄우는법 sprite
 public class PredictBoardManager : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -15,6 +15,7 @@ public class PredictBoardManager : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
         
