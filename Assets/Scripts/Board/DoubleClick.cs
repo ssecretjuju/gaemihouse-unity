f5abd70794f2cc87f5d1895d88349eeec1f6bb54
dsfjsalgjdka <111098085+dsfjsalgjdka@users.noreply.github.com>
2022-11-14 16:47:42 +0900

    1114-유아이수정게시판추가(방만들기버튼연결해줘요)

--- Assets/Scripts/Board/DoubleClick.cs
+++ Assets/Scripts/Board/DoubleClick.cs
@@ -17,28 +17,33 @@ public class DoubleClick : MonoBehaviour
         timeBetweenClicks = 0.2f;
         clickCounter = 0;
         coroutineAllowed = true;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+
+    public void DoubleClick12()
+    {
         if (Input.GetMouseButtonUp(0))
             clickCounter += 1;
 
-        if(clickCounter ==1 && coroutineAllowed)
+        if (clickCounter == 1 && coroutineAllowed)
         {
             firstClickTime = Time.time;
             StartCoroutine(DoubleClickDetection());
         }
     }
-
     private IEnumerator DoubleClickDetection()
     {
         coroutineAllowed = false;
-        while(Time.time < firstClickTime + timeBetweenClicks)
+        while (Time.time < firstClickTime + timeBetweenClicks)
         {
-            if(clickCounter == 2)
+            if (clickCounter == 2)
             {
                 //팝업창 활성화
                 playerInfoWindow.SetActive(true);
