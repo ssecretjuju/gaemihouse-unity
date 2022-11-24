b7ec0d34132d96b8e43ac1e33eea5ad491e1c123
aeju <aejuchoe@naver.com>
2022-11-25 02:48:47 +0900

    아 망해뜸

--- Assets/Scripts/HTTP/HttpRequester.cs
+++ Assets/Scripts/HTTP/HttpRequester.cs
@@ -4,7 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+//HttpRequester : 틀 
 
+//게시물 정보
+[System.Serializable]
+public class RoomData
+{
+    public int roomCode;
+    public string roomTitle;
+    public int roomLimitedNumber;
+    public int roomRegistedNumber;
+    public int roomYield;
+
+}
+
+public class ListenData
+{
+    public string status;
+    public string message;
+    public RoomData[] data;
+}
+
+[Serializable]
+public class RoomDataArray
+{
+    public List<RoomData> data;
+}
 
 public enum RequestType
 {
@@ -13,23 +38,17 @@ public enum RequestType
     PUT,
     DELETE
 }
-
-
 public class HttpRequester : MonoBehaviour
 {
     //url
     public string url;
     //요청 타입 (GET, POST, PUT, DELETE)
     public RequestType requestType;
-
     //Post Data 
     public string postData;//(body)
-
     //응답이 왔을 때 호출해줄 함수 (Action)
     //Action : 함수를 넣을 수 있는 자료형
     public Action<DownloadHandler> onComplete;
-
-
     public string imgUrl;
     ////////////////
     //public Action<UploadHandler> action;
